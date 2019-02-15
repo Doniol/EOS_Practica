154a8925cbb70878d59ef6831103bc4ca852d926
Doniol <danielvaneijk-bos@live.nl>
2019-02-15 10:51:44 +0100

    Create caesar.cpp

---
+++ caesar.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+string caesar(int charnum, string input){
+	string newinput = "";
+	for(int i = 0; i < input.size(); i++){
+		char character = input[i];
+		if(character == 'A' || character == 'B' || character == 'C' || character == 'a' || character == 'b' || character == 'c'){
+			char newchar = character + (26 - charnum);
+			newinput += newchar;
+		} else if(character >= 65 && character <= 90 || character >= 97 && character <= 122) {
+			char newchar = character - charnum;
+			newinput += newchar;
+		} else {
+			newinput += character;
+		}
+	}
+	return newinput;
+}
+
+int main(){
+	string input = "";
+	getline(cin, input);
+	int charnum = 3;
+	string output = caesar(charnum, input);
+	cout << output << "\n";
+}
