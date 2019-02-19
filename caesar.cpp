c2086f71066b4845d9c28eb7455f5e2cf24ff7d9
Doniol <danielvaneijk-bos@live.nl>
2019-02-19 19:28:01 +0100

    .

--- caesar.cpp
+++ caesar.cpp
@@ -7,8 +7,13 @@ string caesar(int charnum, string input){
 	string newinput = "";
 	for(int i = 0; i < input.size(); i++){
 		char character = input[i];
-		if(character == 'A' || character == 'B' || character == 'C' || character == 'a' || character == 'b' || character == 'c'){
+		if(character >= 65 && character <= (65 + charnum) || character >= 97 && character <= (97 + charnum)){
 			char newchar = character + (26 - charnum);
+			if(newchar == 123){
+				newchar = 97;
+			} else if(newchar == 91){
+				newchar = 65;
+			}
 			newinput += newchar;
 		} else if(character >= 65 && character <= 90 || character >= 97 && character <= 122) {
 			char newchar = character - charnum;
@@ -20,10 +25,10 @@ string caesar(int charnum, string input){
 	return newinput;
 }
 
-int main(int argc, char argv[]){
+int main(){
 	string input = "";
 	getline(cin, input);
-	int charnum = argv[1];
+	int charnum = 6;
 	string output = caesar(charnum, input);
 	cout << output << "\n";
 }
