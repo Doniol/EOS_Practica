8f476f0f6fd7d0f932fde052d5c509d2b314e052
Doniol <43216456+Doniol@users.noreply.github.com>
2019-03-11 11:46:06 +0100

    Add files via upload

---
+++ maakLogin.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo "Enter Username: "
+read USER
+if [[ -z "${USER// }" ]]
+then
+	USERNAME=$(whoami)
+else
+	USERNAME=$USER
+fi
+echo "Enter Password (Password must contain 1 uppercase, 1 lowercase letter, 1 number and must be at least 8 characters long): "
+read PASS
+while :
+do
+	if [[ $PASS =~ [A-Z] ]] && [[ $PASS =~ [a-z] ]] && [[ $PASS =~ [0-9] ]] && [ ${#PASS} -gt 8 ]
+	then
+		break
+	fi
+	echo "Invalid Password!"
+	read PASS
+done
+echo "$USERNAME - $PASS" >> /home/hu/$1.txt
+echo "Account details saved in: /home/hu/$1.txt"
